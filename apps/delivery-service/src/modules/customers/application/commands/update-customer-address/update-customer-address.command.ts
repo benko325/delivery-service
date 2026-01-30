0508ea006066d562c6be25c53642cc9913a2cb17
lacopittner <l.pittner9@gmail.com>
2026-01-30 16:47:19 +0100

    refactor(customers): remove deprecated address commands and mappers

--- apps/delivery-service/src/modules/customers/application/commands/update-customer-address/update-customer-address.command.ts
+++
@@ -1,9 +0,0 @@
-import { ICommand } from '@nestjs/cqrs';
-import { CustomerAddress } from '../../../core/types/customer-database.types';
-
-export class UpdateCustomerAddressCommand implements ICommand {
-    constructor(
-        public readonly id: string,
-        public readonly address: CustomerAddress,
-    ) {}
-}
